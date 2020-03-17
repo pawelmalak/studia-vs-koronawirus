6174809b6a4f5c7eea8d2f34a09d36532f81fb99
unknown <pawel999@icloud.com>
2020-03-18 00:36:09 +0100

    Basic frontend functionality

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -2,8 +2,6 @@ import 'dayjs/locale/pl';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-const container = document.querySelector('.container');
-
 const getData = async () => {
 
   const req = await fetch('/classes');
@@ -15,10 +13,45 @@ const getData = async () => {
 dayjs.locale('pl');
 dayjs.extend(relativeTime);
 
-console.log(dayjs('2018-05-05').format());
-console.log(dayjs().to(dayjs('2020-03-17 21:30:00', 'YYYY-MM-DD HH:mm:ss')));
-
 (async () => {
-  console.log(await getData());
+  const data = await getData();
+  console.log(data);
+
+  data.forEach((classInstance, index) => {
+    document.querySelector('#columns-holder').innerHTML += `
+    <div class="col-3">
+      <div class="card">
+        <div class="card-body" id="card-${index}">
+          <h5 class="card-title">${classInstance.class_name}</h5>
+        </div>
+      </div>
+    </div>
+    `;
+    
+    classInstance.class_events.forEach((eventInstance) => {
+      document.querySelector(`#card-${index}`).innerHTML += `
+        <div class="col-12 px-0">
+          <div class="card mt-3">
+            <div class="card-body">
+              <h5 class="card-title">Event <span class="badge badge-${determinePriority(eventInstance.event_deadline)} float-right">${dayjs().to(dayjs(eventInstance.event_deadline, 'YYYY-MM-DD HH:mm:ss'))}</span></h5>
+              <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, aliquid.</p>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+  });
+
 })();
 
+
+const determinePriority = (eventDate) => {
+  const diff = dayjs(eventDate, 'YYYY-MM-DD HH:mm:ss').diff(dayjs(), 'day');
+  
+  if (diff > 15) return 'success';
+  else if (diff >= 7) return 'info';
+  else if (diff >= 3) return 'warning';
+  else if (diff >= 0) return 'danger';
+  
+}
